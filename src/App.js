95ba4c1b1e872876b3f69da054a46e3bf5029e04
rc-li <ruicheng.li@uconn.edu>
2020-02-19 13:03:25 -0800

    have a barebone of todo list
    70% through the 30mins learn react tutorial.
    Didn't encounter bug, but through the experience, I know that a lot of concepts about JS and HTML need to be explored before I can really do anything with it
    
    just commit before our lunch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import TodoList from './TodoList';
+import uuidv4 from 'uuid/v4'
 
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
-  return null
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value
+    if (name === '') return
+    setTodos(prevTodos => {
+      return [...prevTodos, {id: uuidv4(), name: name, complete:
+      false}]
+    })
+    todoNameRef.current.value = null
+  }
+
+  return (
+    <>
+      <TodoList todos={todos} />
+      <input ref={todoNameRef} type="text" />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <button>Clear Completed Todo</button>
+      <div>0 left to do</div>
+    </>
+  )
 }
 
 export default App;
