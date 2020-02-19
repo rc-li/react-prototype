a592e1a910f8560bb012b71eab4c557dc2d5b2da
rc-li <ruicheng.li@uconn.edu>
2020-02-19 14:38:33 -0800

    completed todo list tutorial

--- src/App.js
+++ src/App.js
@@ -9,8 +9,21 @@ function App() {
   const todoNameRef = useRef()
 
   useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem
+      (LOCAL_STORAGE_KEY))
+      if (storedTodos) setTodos(storedTodos)
+  },[])
+
+  useEffect(() => {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
   }, [todos])
+
+  function toggleTodo(id) {
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.complete = !todo.complete
+    setTodos(newTodos)
+  }
 
   function handleAddTodo(e) {
     const name = todoNameRef.current.value
@@ -22,13 +35,18 @@ function App() {
     todoNameRef.current.value = null
   }
 
+  function handleClearTodos() {
+    const newTodos = todos.filter(todo => !todo.complete)
+    setTodos(newTodos)
+  }
+
   return (
     <>
-      <TodoList todos={todos} />
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
       <input ref={todoNameRef} type="text" />
       <button onClick={handleAddTodo}>Add Todo</button>
-      <button>Clear Completed Todo</button>
-      <div>0 left to do</div>
+      <button onClick={handleClearTodos}>Clear Completed Todo</button>
+      <div>{todos.filter(todo => !todo.complete).length} left to do</div>
     </>
   )
 }
