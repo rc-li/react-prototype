43b604b16f42d468d117c160ad0e817bffab13d2
rc-li <ruicheng.li@uconn.edu>
2020-02-19 16:45:02 -0800

    Updates

---
+++ precache-manifest.7320bd2bc92abf06237f8506d84a45e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b6ac88937f7118fa7b8fa0115fdc826",
+    "url": "/react-prototype/index.html"
+  },
+  {
+    "revision": "eae66776b75a05fddce1",
+    "url": "/react-prototype/static/js/2.14311d34.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-prototype/static/js/2.14311d34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57ea6206f1f66990bbed",
+    "url": "/react-prototype/static/js/main.01782742.chunk.js"
+  },
+  {
+    "revision": "a63903b04b9177580c8c",
+    "url": "/react-prototype/static/js/runtime-main.0b793e9b.js"
+  }
+]);
